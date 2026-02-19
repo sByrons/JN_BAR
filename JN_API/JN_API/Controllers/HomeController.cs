bcd7a2aa4bf9e5c8215865a8ee62cc6911f18be8
sByrons <109484176+sByrons@users.noreply.github.com>
2026-02-18 19:42:06 -0600

    Iniciar Sesion en el API

--- JN_API/JN_API/Controllers/HomeController.cs
+++ JN_API/JN_API/Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using Dapper;
 using JN_API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using Microsoft.VisualBasic;
 
 namespace JN_API.Controllers
 {
@@ -19,23 +20,38 @@ namespace JN_API.Controllers
 		[HttpPost("Registro Usuario")]
         public IActionResult RegistroUsuario(Usuario model)
         {
-            using (var context = new SqlConnection(_config.GetValue<string>("ConnectionStrings:DefaultConnection")))
-            {
-                var parametros = new DynamicParameters();
-                parametros.Add("@Indentificacion", model.Indentificacion);
-                parametros.Add("@Nombre", model.Nombre);
-                parametros.Add("@CorreoElectronico", model.CorreoElectronico);
-                parametros.Add("@Contrasenna", model.Contrasenna);
+			using var context = new SqlConnection(_config.GetValue<string>("ConnectionStrings:DefaultConnection"));
+			var parametros = new DynamicParameters();
+			parametros.Add("@Indentificacion", model.Indentificacion);
+			parametros.Add("@Nombre", model.Nombre);
+			parametros.Add("@CorreoElectronico", model.CorreoElectronico);
+			parametros.Add("@Contrasenna", model.Contrasenna);
 
-                var result = context.Execute("sp_RegistrarCuenta", parametros);
+			var result = context.Execute("sp_RegistrarCuenta", parametros);
 
-                if (result <= 0)
-                    return BadRequest(new { message = "No se registró su información" });
+			if (result <= 0)
+				return BadRequest(new { message = "No se registró su información" });
 
-                return Ok(new {message = "Su información se registró correctamente"});
-            }
-           
-     
+			return Ok(new { message = "Su información se registró correctamente" });
+		}
+
+        [HttpPost("IniciarSesion")]
+
+        public IActionResult IniciarSesion(Usuario model)
+        {
+			using var context = new SqlConnection(_config.GetValue<string>("ConnectionStrings:DefaultConnection"));
+			var parametros = new DynamicParameters();
+			parametros.Add("@CorreoElectronico", model.CorreoElectronico);
+			parametros.Add("@Contrasenna", model.Contrasenna);
+
+			var result = context.QueryFirstOrDefault<Usuario>("sp_IniciarSesion", parametros);
+
+			if (result == null)
+				return NotFound("Su información no se autentico correctamente");
+
+			return Ok("Su información se autentico correctamente");
+
+			
         }
 
 	}
